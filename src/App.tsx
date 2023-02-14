d92345d466e1e93ae3356d80c4ee385dab1a51e0
Mario Montero <mario.montero@devitech.com.co>
2023-02-13 20:07:12 -0500

    adding firebase

---
+++ src/App.tsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import { getUniqueId, getManufacturer } from 'react-native-device-info';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+import {
+  Colors,
+  Header,
+} from 'react-native/Libraries/NewAppScreen';
+import Home from './views/Home';
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  React.useEffect(() => {
+    async function getUniqueId(): Promise<string> {
+      const uniqueId:any = await getUniqueId();
+      const manufacturer = await getManufacturer();
+      console.log('uniqueId', uniqueId);
+      console.log('manufacturer', manufacturer);
+      return uniqueId;
+    }
+
+    getUniqueId();
+    
+    const userDocument = firestore().collection('Users').doc('ABC');
+  }, []);
+
+
+  return (
+
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <Home/>
+        {/* <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View> */}
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
---
+++ src/views/initialPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const InitialPage = () => {
+  return (
+    <View>
+        <Text>
+            This is the home page
+        </Text>
+    </View>
+  )
+}
+
+export default InitialPage
